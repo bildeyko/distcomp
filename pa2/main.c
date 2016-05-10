c1c4a819deb430548f5f0cc452f7ef4e0174ec3f
Nikolay Bildeyko <bildeyko@gmail.com>
2016-05-11 00:30:11 +0300

    Final version

--- pa2/main.c
+++ pa2/main.c
@@ -8,28 +8,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <time.h>
 #include <string.h>
 #include <wait.h>
+#include <time.h>
 
 #include "ipc.h"
 #include "common.h"
-#include "pa1.h"
+#include "pa2345.h"
+#include "main.h"
+#include "banking.h"
 
-struct pipes_t
-{
-   int rdwr[2]; // 0 = read, 1 = write
-};
-
-struct dataIO_t
-{
-	int processes;
-	int8_t lid;
-	struct pipes_t pipes[MAX_PROCESS_ID][MAX_PROCESS_ID];
-};
 
 void usage();
-int closeUnusedPipes(void * self);
+int closeUnusedPipes(void *);
+void doChild(void *, FILE *, int, int);
 
 int main(int argc, char *argv[])
 {
@@ -52,8 +44,10 @@ int main(int argc, char *argv[])
 			default:
 				usage();
 		}
-	}	
-	
+	}
+	argc -= optind;
+	argv += optind;	
+
 	data.lid = 0;
 
 	if ((fd_pipes = fopen(pipes_log, "w")) == NULL) {
@@ -73,8 +67,12 @@ int main(int argc, char *argv[])
 					exit(1);
 				}
 
-				int mode = fcntl(data.pipes[i][j].rdwr[0], F_GETFL);
+				int mode;
+				mode = fcntl(data.pipes[i][j].rdwr[0], F_GETFL);
 				fcntl(data.pipes[i][j].rdwr[0], F_SETFL, mode | O_NONBLOCK);
+
+				mode = fcntl(data.pipes[i][j].rdwr[1], F_GETFL);
+				fcntl(data.pipes[i][j].rdwr[1], F_SETFL, mode | O_NONBLOCK);
 
 				fprintf(fd_pipes, "The pipe %d ===> %d was created\n", j, i);
 			}
@@ -99,63 +97,14 @@ int main(int argc, char *argv[])
 			fprintf(stderr, "Error creating the child\n");
 			exit(1);
 		} else if (pid == 0) {
-			
-			start_msgs = data.processes - 2;
-			done_msgs = data.processes - 2;
-
-			data.lid = i;
-
-			closeUnusedPipes(&data);
-		
-			fprintf(fd_events, log_started_fmt, data.lid, getpid(), getppid());
-			fflush(fd_events);
-			printf(log_started_fmt, data.lid, getpid(), getppid());
-			
-			msg.s_header.s_type = STARTED;
-			sprintf(msg.s_payload, log_started_fmt, data.lid, getpid(), getppid());
-			msg.s_header.s_payload_len = strlen(msg.s_payload);
-			send_multicast(&data, &msg);
-
-			while(start_msgs) {				
-				if(receive_any(&data, &resMsg) == 0) {
-					if(resMsg.s_header.s_type == STARTED)
-						start_msgs--;
-					if(resMsg.s_header.s_type == DONE)
-						done_msgs--;
-				}
-			}
-
-			fprintf(fd_events, log_received_all_started_fmt, data.lid);
-			fflush(fd_events);
-			printf(log_received_all_started_fmt, data.lid);
-
-			fprintf(fd_events, log_done_fmt, data.lid);
-			fflush(fd_events);
-			printf(log_done_fmt, data.lid);			
-
-			msg.s_header.s_type = DONE;
-			sprintf(msg.s_payload, log_done_fmt, data.lid);
-			msg.s_header.s_payload_len = strlen(msg.s_payload);
-			send_multicast(&data, &msg);
-
-			while(done_msgs) {				
-				if(receive_any(&data, &resMsg) == 0) {
-					if(resMsg.s_header.s_type == DONE)
-						done_msgs--;
-				}
-			}
-
-			fprintf(fd_events, log_received_all_done_fmt, data.lid);
-			fflush(fd_events);
-			printf(log_received_all_done_fmt, data.lid);
-
-			fclose(fd_events);
+			doChild(&data, fd_events, i, atoi(argv[i-1]));
 			exit(0); 
 		}
 	}
 
 	closeUnusedPipes(&data);
 
+	timestamp_t tm = get_physical_time();
 	start_msgs = data.processes - 1;
 	done_msgs = data.processes - 1;
 
@@ -168,9 +117,17 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	fprintf(fd_events, log_received_all_started_fmt, data.lid);
+	tm = get_physical_time();
+
+	fprintf(fd_events, log_received_all_started_fmt, tm, data.lid);
 	fflush(fd_events);
-	printf(log_received_all_started_fmt, data.lid);
+	printf(log_received_all_started_fmt, tm, data.lid);
+
+	bank_robbery(&data,data.processes-1);
+
+	msg.s_header.s_type = STOP;
+	msg.s_header.s_payload_len = 0;
+	send_multicast(&data, &msg);
 
 	while(done_msgs) {				
 		if(receive_any(&data, &resMsg) == 0) {
@@ -179,76 +136,190 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	fprintf(fd_events, log_received_all_done_fmt, data.lid);
+	tm = get_physical_time();
+
+	fprintf(fd_events, log_received_all_done_fmt, tm, data.lid);
 	fflush(fd_events);
-	printf(log_received_all_done_fmt, data.lid);		
+	printf(log_received_all_done_fmt, tm, data.lid);
+
+	int history_msgs = data.processes - 1;
+	AllHistory allHistory;
+	allHistory.s_history_len = history_msgs;
+
+	do {				
+		if(receive_any(&data, &resMsg) == 0) {
+			BalanceHistory hist;
+			memcpy(&hist, resMsg.s_payload, resMsg.s_header.s_payload_len);
+
+			if(resMsg.s_header.s_type == BALANCE_HISTORY)
+			{	
+				allHistory.s_history[hist.s_id-1] = hist;
+				history_msgs--;
+			}
+		}
+	} while(history_msgs);
+
+	print_history(&allHistory);
 
 	fclose(fd_events);
 
 	for(int i = 0; i < data.processes; i++) {
-		wait(&i);
+		wait(NULL);
 	}	
 
 	return 0;
 }
 
-int send(void * self, local_id dst, const Message * msg) {
-	struct dataIO_t* data = self;
-	uint16_t size = sizeof(msg->s_header) + msg->s_header.s_payload_len;
+void doChild(void *parentData, FILE *fd_events, int lid, int initBalance)
+{
+	struct dataIO_t* data = parentData;
 
-	if(write(data->pipes[dst][data->lid].rdwr[1], msg, size) != size)
-		return 1;
-	return 0;
-}
+	data->lid = lid;
+	int done_msgs = data->processes - 2;
+	Message msg, resMsg;
 
-int send_multicast(void * self, const Message * msg) {
-	struct dataIO_t* data = self;
+	BalanceHistory history;
+	history.s_id = lid;
+	history.s_history_len = 0;		
 
-	for(int i = 0; i < data->processes; i++) {
-		if(i == data->lid)
-			continue;
-		if(send(self, i, msg) != 0)
-			return 1;
-	}
-	return 0;
-}
+	timestamp_t tm = get_physical_time();
 
-int receive(void * self, local_id from, Message * msg) {
-	struct dataIO_t* data = self;
-	uint16_t size = sizeof(msg->s_header);
+	balance_t childBalance = initBalance;
 
-	if(read(data->pipes[data->lid][from].rdwr[0], &msg->s_header, size) != size)
-		return -1;
+	history.s_history[history.s_history_len].s_balance = childBalance;
+	history.s_history[history.s_history_len].s_time = tm;
+	history.s_history[history.s_history_len].s_balance_pending_in = 0;			
 
-	size = msg->s_header.s_payload_len;
+	closeUnusedPipes(data);
 
-	if(read(data->pipes[data->lid][from].rdwr[0], &msg->s_payload, size) != size)
-		return -1;
+	fprintf(fd_events, log_started_fmt, tm, history.s_id, getpid(), getppid(), 
+			history.s_history[history.s_history_len].s_balance);
+	fflush(fd_events);
+	printf(log_started_fmt, tm, history.s_id, getpid(), getppid(), 
+		   history.s_history[history.s_history_len].s_balance);
+	
+	msg.s_header.s_type = STARTED;
+	msg.s_header.s_magic = MESSAGE_MAGIC;
+	sprintf(msg.s_payload, log_started_fmt, tm, history.s_id, getpid(), 
+			getppid(), history.s_history[history.s_history_len].s_balance);
+	msg.s_header.s_payload_len = strlen(msg.s_payload);
+	send(data, 0, &msg);
 
-	return 0;
-}
+	history.s_history_len ++;
 
-int receive_any(void * self, Message * msg) {
-	struct dataIO_t* data = self;
+	while(1 && done_msgs) {
+		tm = get_physical_time();
 
-	for(int i = 0; i < data->processes; i++) {
-		if(i == data->lid)
-			continue;
-		if(receive(self, i, msg) == 0)
-			return 0;
+		/*
+			Set balance for empty timestamps
+		*/	
+		BalanceState balance;
+		balance.s_balance_pending_in = 0;
+		balance.s_balance = childBalance;
+		balance.s_time = get_physical_time();
+		history.s_history[history.s_history_len] = balance;
+		history.s_history_len ++;
+
+		if(receive_any(data, &resMsg) == 0) {
+			if(resMsg.s_header.s_type == DONE) {
+				done_msgs--;	
+			}
+			if(resMsg.s_header.s_type == STOP) {
+				msg.s_header.s_type = DONE;
+				sprintf(msg.s_payload, log_done_fmt, tm, data->lid, childBalance);
+				msg.s_header.s_payload_len = strlen(msg.s_payload);
+				send_multicast(data, &msg);
+
+				fprintf(fd_events, log_done_fmt, tm, data->lid, childBalance);
+				fflush(fd_events);
+				printf(log_done_fmt, tm, data->lid, childBalance);
+			}
+			if(resMsg.s_header.s_type == TRANSFER) {
+				TransferOrder order;
+				memcpy(&order, resMsg.s_payload, resMsg.s_header.s_payload_len);
+
+				if(order.s_src == data->lid) {
+					fprintf(fd_events, log_transfer_out_fmt, tm, data->lid, 
+							order.s_amount, order.s_dst);
+					fflush(fd_events);
+					printf(log_transfer_out_fmt, tm, data->lid, order.s_amount, 
+						   order.s_dst);
+					send(data, order.s_dst, &resMsg);
+
+					childBalance -= order.s_amount;
+
+					BalanceState balance;
+					balance.s_balance_pending_in = 0;
+					balance.s_balance = childBalance;
+					balance.s_time = tm;
+
+					history.s_history[history.s_history_len] = balance;
+				}
+				if(order.s_dst == data->lid) {
+					fprintf(fd_events, log_transfer_in_fmt, tm, data->lid, 
+							order.s_amount, order.s_src);
+					fflush(fd_events);
+					printf(log_transfer_in_fmt, tm, data->lid, order.s_amount, 
+						   order.s_src);
+
+					childBalance += order.s_amount;
+
+					BalanceState balance;
+					balance.s_balance_pending_in = 0;
+					balance.s_balance = childBalance;
+					balance.s_time = tm;
+
+					history.s_history[history.s_history_len] = balance;
+
+					msg.s_header.s_type = ACK;
+					msg.s_header.s_magic = MESSAGE_MAGIC;
+					msg.s_header.s_local_time = tm;
+					msg.s_header.s_payload_len = 0;
+					send(data, 0, &msg);
+				}
+			}
+		}
 	}
 
 	struct timespec tmr;
 	tmr.tv_sec = 0;
 	tmr.tv_nsec = 50000000;
+	nanosleep(&tmr, NULL);
 
-	if(nanosleep(&tmr, NULL) < 0 )   
-	{
-		printf("Nano sleep system call failed \n");
-		return -1;
+	msg.s_header.s_magic = MESSAGE_MAGIC;
+	msg.s_header.s_type = BALANCE_HISTORY;
+	msg.s_header.s_local_time = get_physical_time();
+	msg.s_header.s_payload_len = sizeof(history);
+	memcpy(msg.s_payload, &history, sizeof(history));	
+
+	send(data, 0, &msg);
+
+	fclose(fd_events);
+}
+
+void transfer(void * parent_data, local_id src, local_id dst,
+              balance_t amount)
+{
+    TransferOrder order;
+    order.s_src = src;
+    order.s_dst = dst;
+    order.s_amount = amount;
+
+    Message msg, resMsg;
+    msg.s_header.s_magic = MESSAGE_MAGIC;
+	msg.s_header.s_type = TRANSFER;
+	msg.s_header.s_local_time = get_physical_time();
+	msg.s_header.s_payload_len = sizeof(order);
+	memcpy(msg.s_payload, &order, sizeof(order));	
+
+	send(parent_data, src, &msg);
+
+	while(1) {			
+		if(receive(parent_data, dst, &resMsg) == 0) {
+			if(resMsg.s_header.s_type == ACK)
+				break;			
+		}
 	}
-
-	return 1;
 }
 
 int closeUnusedPipes(void * self)
